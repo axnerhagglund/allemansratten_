5a7aba166b98f55695e25b86983adbd52738e423
axnerhagglund <axnerhagglund@gmail.com>
2025-09-05 11:19:35 +0200

    homepage slider done

--- src/components/SectionFour/SectionFour.jsx
+++ src/components/SectionFour/SectionFour.jsx
@@ -3,7 +3,7 @@ import "./sectionfour.css"
 function SectionFour() {
   return (
     <section className="sectionFour-container">
-      
+      <h1>section four</h1>
     </section>
   )
 }
--- src/components/SectionThree/SectionThree.jsx
+++ src/components/SectionThree/SectionThree.jsx
@@ -3,7 +3,8 @@ import "./sectionThree.css"
 function SectionThree() {
   return (
     <section className="sectionThree-container">
-
+      <h1>Leave the place as you found it</h1>
+      <p>When you leave, please take away your trash, and if found some please take it with you. </p>
     </section>
   )
 }
